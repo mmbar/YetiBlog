164ae965dc37fee57c4d7d3bf70d052ef70ade74
Michał Be <kcwryeti@gmail.com>
2018-06-15 11:33:53 +0200

    Added password change functionality

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\UserPasswordForm;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends Controller
 {
@@ -16,6 +20,35 @@ class UserController extends Controller
         $user = $this->getUser();
         return $this->render('user/user.account.html.twig', [
             'user' => $user
+        ]);
+    }
+    /**
+     * @Route("/userpass", name="user_password_change")
+     */
+    public function changePasswordAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserPasswordForm::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            if ($passwordEncoder->isPasswordValid($user,$data['password'])){
+                $user->setPlainPassword($data['plainPassword']);
+                $em = $this->getDoctrine()->getManager()->flush();
+                return $this->redirectToRoute('user_account_page');
+            }
+
+            $form->get('password')->addError(new FormError('Nieprawidłowe hasło'));
+        }
+
+        return $this->render('user/password.reset.html.twig',[
+            'form' => $form->createView(),
         ]);
     }
 }
