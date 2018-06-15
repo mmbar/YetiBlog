a39c53539f7a383c5d4cf891968fc803180c9ddb
Michał Be <kcwryeti@gmail.com>
2018-06-15 10:14:07 +0200

    Added LoginForm

---
+++ src/AppBundle/Form/LoginForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password', PasswordType::class)
+        ;
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+}
