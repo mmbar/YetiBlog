12386923e1bc88ec3bb123b0d9f15067b2bb37e0
Michał Be <kcwryeti@gmail.com>
2018-06-15 10:24:23 +0200

    Created User entity

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,299 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="app_users")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @UniqueEntity("username")
+ * @UniqueEntity("email")
+ * @Serializer\ExclusionPolicy("all")
+ *
+ */
+class User implements AdvancedUserInterface, \Serializable, EquatableInterface
+{
+    //<editor-fold desc="Fields">
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     *
+     * @Serializer\Expose
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(min="3")
+     *
+     * @Serializer\Expose
+     */
+    protected $username;
+
+    /**
+     * @ORM\Column(type="string", length=64, unique=true)
+     * @Assert\Email()
+     * @Assert\NotBlank()
+     *
+     * @Serializer\Expose
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    protected $password;
+
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean", nullable=true)
+     *
+     * @Serializer\Expose
+     */
+    protected $isActive;
+
+    /**
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     *
+     * @Serializer\Expose
+     */
+    protected $createdAt;
+
+    /**
+     * @ORM\Column(type="json_array", nullable=true)
+     *
+     * @Serializer\Expose
+     */
+    protected $roles = [];
+
+    /**
+     * @var string
+     * @Assert\Length(min="1")
+     */
+    protected $plainPassword;
+    //</editor-fold>
+
+    public function __construct()
+    {
+    }
+
+    public function addRole(string $role)
+    {
+        if (!in_array($role,$this->roles)) {
+            $this->roles[] = $role;
+        }
+    }
+
+    //<editor-fold desc="Getters">
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Setters">
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param mixed $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    }
+
+    /**
+     * @param mixed $roles
+     */
+    public function setRoles(array $roles)
+    {
+        $this->roles = $roles;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+
+
+
+    /**
+     * @param string $plainPassword
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+
+        $this->password = null;
+    }
+    //</editor-fold>
+
+
+    //<editor-fold desc="Implement Methods">
+    public function getRoles()
+    {
+        $roles = $this->roles;
+
+        if (!in_array('ROLE_USER', $roles)) {
+            $roles[] = 'ROLE_USER';
+        }
+        return $roles;
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function eraseCredentials()
+    {
+        $this->plainPassword = null;
+    }
+
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    public function isEqualTo(UserInterface $user)
+    {
+        if ($this->password !== $user->getPassword()) {
+            return false;
+        }
+
+
+        if ($this->username !== $user->getUsername()) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+        ));
+    }
+
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            ) = unserialize($serialized, ['allowed_classes' => false]);
+    }
+    //</editor-fold>
+
+}
+
