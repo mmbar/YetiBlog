c8af250746d1ab4f23aa1f7cc16411e242aae35d
Michał Be <kcwryeti@gmail.com>
2018-06-15 11:00:05 +0200

    Added CreateUserListener

---
+++ src/AppBundle/Doctrine/CreateUserListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Doctrine;
+
+
+use AppBundle\Entity\User;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class CreateUserListener implements EventSubscriber
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return ['prePersist'];
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (!$entity instanceof User) {
+            return;
+        }
+
+        $entity->setIsActive(true);
+        $entity->addRole('ROLE_USER');
+        $entity->setCreatedAt(new \DateTime());
+    }
+}
